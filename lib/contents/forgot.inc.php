2f933fe5fe0ba898eb4c5eda20c2431524b401c9
Jushadi Arman Saz <czar.hadi@gmail.com>
2025-09-02 18:50:11 +0800

    Update forgot.inc.php

--- lib/contents/forgot.inc.php
+++ lib/contents/forgot.inc.php
@@ -18,10 +18,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  *
+ * Modified by Jushadi Arman Saz 202509021600
  */
 
 use SLiMS\Url;
-use SLiMS\Http\Client;
 use SLiMS\Captcha\Factory as Captcha;
 
 // be sure that this file not accessed directly
@@ -30,12 +30,6 @@ if (!defined('INDEX_AUTH')) {
 } elseif (INDEX_AUTH != 1) { 
     die("can not access this file directly");
 }
-
-/*
-if (defined('LIGHTWEIGHT_MODE')) {
-    header('Location: index.php');
-}
-*/
 
 // required file
 require SIMBIO.'simbio_DB/simbio_dbop.inc.php';
@@ -48,57 +42,95 @@ if ($sysconf['https_enable']) {
 // Captcha initialize
 $captcha = Captcha::section('forgot');
 
+// Variable to hold feedback script
+$feedback_script = '';
+
 // start the output buffering for main content
 ob_start();
 
 if (isset($_POST['resetPass'])) {
     $email = $dbs->escape_string($_POST['currentmail']);
     if (!$email) {
-        echo '<script type="text/javascript">alert(\''.__('Please supply valid username and password').'\');</script>';
+        $message = addslashes(__('Please enter a valid email address.'));
+        $feedback_script = "<script>toastr.error('{$message}');</script>";
     } else {
-        # <!-- Captcha form processing - start -->
-        if ($captcha->isSectionActive()) {    
-            try {
-                if ($captcha->isValid() === false) throw new Exception(__('Captcha incorrect.'));
-                    
-                // Validate current email
-                $_q = $dbs->query("SELECT user_id, realname FROM user WHERE email='{$email}'");
-
-                if ($_q->num_rows === 0) throw new Exception(__('Current email not found. Please try again.'));
+        try {
+            if ($captcha->isSectionActive() && $captcha->isValid() === false) {
+                throw new Exception(__('Captcha incorrect.'));
+            }
                 
-                // Name
+            // Validate current email
+            $_q = $dbs->query("SELECT user_id, realname FROM user WHERE email='{$email}'");
+
+            if ($_q->num_rows === 0) {
+                // Email not found, provide generic success message for security
+                // This prevents attackers from guessing registered emails
+                $message = addslashes(__('If a matching account was found, an email has been sent to reset your password.'));
+                $feedback_script = "<script>toastr.info('{$message}');</script>";
+            } else {
+                // Email found, proceed to send reset link
                 $file_d = $_q->fetch_assoc();
                 $name = $file_d['realname'];
-                /// Generate a token for forgot password
-                $salt = password_hash($email, PASSWORD_DEFAULT);
-                $_sql_update_salt = sprintf("UPDATE user SET forgot = '{$salt}', last_update = CURDATE() WHERE email = '%s'", $email);
-                // write log
+                $salt = bin2hex(random_bytes(32));
+                $_sql_update_salt = sprintf("UPDATE user SET forgot = '{$salt}', last_update = NOW() WHERE email = '%s'", $email);
                 writeLog('staff', $name, 'Forgot Password', $name.' has been requested a new password.', 'Password', 'Request');
                 $_update_q = $dbs->query($_sql_update_salt);
-                
+                    
+                /*
+                 * =================================================================
+                 * KODE LAMA (DIJADIKAN KOMENTAR)
+                 * Kode ini bergantung pada layanan eksternal slims.web.id
+                 * =================================================================
+                 *
                 // force scheme to https
-                if (Url::getPort() == '443') Url::$forceHttps = true;
+                // if (Url::getPort() == '443') Url::$forceHttps = true;
 
                 // set hook process variable
-                $hookProcess = Client::withHeaders([
-                    "X-API-KEY" => $salt
-                ])->post('https://slims.web.id/mailer/forgot.php', [
-                    'url' => (string)Url::getSlimsFullUri(),
-                    'salt' => $salt,
-                    'email' => $email,
-                    'name' => $name
-                ]);
+                $hookProcess = Client::withHeaders(["X-API-KEY" => $salt])
+                    ->post('https://slims.web.id/mailer/forgot.php', [
+                        'url' => (string)Url::getSlimsFullUri(), 'salt' => $salt,
+                        'email' => $email, 'name' => $name
+                    ]);
 
                 if ($hookProcess->getStatusCode() !== 200 || $hookProcess->getContent() == 'false') {
                     $error = (isDev() ? ' ' . __('Error') . ' : ' . $hookProcess->getError() : ' ' . __('Error not available'));
                     throw new Exception(__('Cannot send the email. Please try again.') . $error);
                 }
-                    
-                flash('resetSuccess', __('<strong>Congratulations! </strong>An instruction has been sent to your email. Please check your inbox.'));
-            } catch (Exception $e) {
-                flash('resetFailed', $e->getMessage());
+                */
+
+                // =================================================================
+                // KODE BARU (PERBAIKAN) - Menggunakan setelan email lokal SLiMS
+                // =================================================================
+                require_once LIB . 'PHPMailer/src/Exception.php';
+                require_once LIB . 'PHPMailer/src/PHPMailer.php';
+                require_once LIB . 'PHPMailer/src/SMTP.php';
+                
+                $mail = SLiMS\Mail::getInstance();
+                $mail->addAddress($email, $name);
+                
+                $mail->Subject = $sysconf['library_name'] . ' - ' . __('Reset Password Instruction');
+                $resetLink = rtrim(Url::getSlimsBaseUri(), '/') . '/index.php?p=newpass&token=' . urlencode($salt);
+                
+                $mail->Body = sprintf(__('Hi %s,'), $name) . "\n\n";
+                $mail->Body .= __('You have requested a password reset. Please click the link below to create a new password:') . "\n";
+                $mail->Body .= $resetLink . "\n\n";
+                $mail->Body .= __('If you did not request this, please ignore this email.') . "\n\n";
+                $mail->Body .= __('Regards,') . "\n";
+                $mail->Body .= $sysconf['library_name'];
+                
+                if (!$mail->send()) {
+                    throw new Exception(__('Cannot send the email.') . ' Mailer Error: ' . $mail->ErrorInfo);
+                }
+
+                $message = addslashes(__('<strong>Congratulations!</strong> An instruction has been sent to your email. Please check your inbox.'));
+                $feedback_script = "<script>toastr.success('{$message}');</script>";
             }
+
+        } catch (Exception $e) {
+            $message = addslashes($e->getMessage());
+            $feedback_script = "<script>toastr.error('{$message}');</script>";
         }
+        
     }
 }
 ?>
@@ -107,14 +139,7 @@ if (isset($_POST['resetPass'])) {
         <div style="font-weight: bold; color: #FF0000;"><?php echo __('Your browser does not support Javascript or Javascript is disabled. Application won\'t run without Javascript!'); ?><div>
     </noscript>
     <div class="mb-3">
-        <?php 
-        if (flash()->isEmpty()) {
-            // if there is login action
-            echo __('If you need help resetting your password, we can help by sending you a link to reset it.');
-        } else if ($key = flash()->includes('resetFailed','resetSuccess')) {
-            flash()->show($key);
-        }
-        ?>
+        <?php echo __('If you need help resetting your password, we can help by sending you a link to reset it.'); ?>
     </div>
     <form action="index.php?p=forgot" method="post" novalidation>
         <div class="heading1"><?php echo __('Your email address'); ?></div>
@@ -132,6 +157,10 @@ if (isset($_POST['resetPass'])) {
         <a class="forgotButton" href="index.php?p=login"><?php echo __('Cancel') ?></a>
         </div>
     </form>
+    <?php 
+    // Menampilkan script notifikasi toast setelah form
+    echo $feedback_script; 
+    ?>
 </div>
 <script type="text/javascript">jQuery('#currentmail').focus();</script>
 
@@ -142,6 +171,7 @@ $main_content = ob_get_clean();
 // page title
 $page_title = __('Forgot My Password').' | '.$sysconf['library_name'];
 
+// Memuat template
 if ($sysconf['template']['base'] == 'html') {
     // create the template object
     $template = new simbio_template_parser($sysconf['template']['dir'].'/'.$sysconf['template']['theme'].'/login_template.html');
@@ -154,4 +184,5 @@ if ($sysconf['template']['base'] == 'html') {
 } else if ($sysconf['template']['base'] == 'php') {
     require_once $sysconf['template']['dir'].'/'.$sysconf['template']['theme'].'/login_template.inc.php';
 }
+
 exit();
